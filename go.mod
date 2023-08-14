d09489e7c65baa6ba8fafa2184149ebed3d688de
Thomas Kienlen <kommander@laposte.net>
2023-08-14 22:37:36 +0200

    tmp

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/kmmndr/goPi
 
 go 1.20
-
-replace github.com/luismesas/goPi v0.0.0 => github.com/kmmndr/goPi v1.0.2
--- goPi.go
+++ goPi.go
@@ -1,11 +1,10 @@
-
 // Package rpi is a library for interfacing with Raspberry Pi IO
-package rpi
+package goPi
 
-// This is the wrong url here ??? 
+// This is the wrong url here ???
 import (
 	_ "github.com/kmmndr/goPi/MCP23S17"
 	_ "github.com/kmmndr/goPi/ioctl"
-	_ "github.com/kmmndr/goPi/spi"
 	_ "github.com/kmmndr/goPi/piface"
+	_ "github.com/kmmndr/goPi/spi"
 )
